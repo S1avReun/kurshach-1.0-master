bfb0da1179c1afd4f534833b3ea5db3f2db9dc8d
S1avReun <is109-petrov@urtt.ru>
2022-11-25 19:19:18 +1400

    boost

--- taskmanage/main/urls.py
+++ taskmanage/main/urls.py
@@ -5,4 +5,8 @@ from django.views.generic.base import TemplateView
 urlpatterns = [
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
     path('about/', views.aboutPageView, name='about'),
+    path('contacts/', views.contactsPageView, name='contacts'),
+    path('prices/', views.pricesPageView, name='prices'),
+    path('services/', views.servicesPageView, name='services'),
+    path('product/', views.productPageView, name='product'),
 ]
--- taskmanage/main/views.py
+++ taskmanage/main/views.py
@@ -6,3 +6,17 @@ def homePageView(request):
 
 def aboutPageView(request):
     return render(request, 'about.html')
+
+def servicesPageView(request):
+    return render(request, 'services.html')
+
+def contactsPageView(request):
+    return render(request, 'contacts.html')
+
+def pricesPageView(request):
+    return render(request, 'prices.html')
+
+def productPageView(request):
+    return render(request, 'product.html')
+
+
